33b0b469ed11d79b38c38e175fa32ae0f5add5e0
EukirneRas <henriquedeandraderodrigues@gmail.com>
2022-07-22 11:45:28 -0300

    Polimorfismo

---
+++ Polimorfismo2/Polimorfismo2/Src/Area.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Polimorfismo2.Src
+{
+        class Area
+        {
+        private double @base;
+        private double altura;
+        private double lado;
+
+        public double _Base { get; set; }
+            public double _Altura { get; set; }
+            public double _Lado { get; set; }
+
+            public Area(double _base, double _altura, double _lado)
+        {
+            _Base = _base;
+            _Altura = _altura;
+            _Lado = _lado;
+        }
+
+        public Area(double @base, double altura)
+        {
+            this.@base = @base;
+            this.altura = altura;
+        }
+
+        public Area(double lado)
+        {
+            this.lado = lado;
+        }
+    }
+}
